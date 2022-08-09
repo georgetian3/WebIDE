5ea3b95c2ebec06eba8bdb25179db87cbfd0e7fc
ChimpOnCloud <sunfy20@mails.tsinghua.edu.cn>
2022-08-09 18:22:50 +0800

    elementui included

---
+++ webide/src/components/welcomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="welcome">
+    <el-row>
+      <el-col :span="24">
+        <div class="welcome">Welcome to our online IDE!</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .el-row {
+    margin-bottom: 20px;
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .welcome {
+    background: #99a9bf;
+    border-radius: 10px;
+    min-height: 20px;
+
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: red;
+  }
+</style>
