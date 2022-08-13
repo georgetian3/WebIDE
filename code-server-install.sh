9fb02416ce4e59a852cf537f57fb1073ef6f2967
George Tian <github@georgetian.com>
2022-08-13 17:13:50 +0800

    Create code-server-install.sh

---
+++ code-server-install.sh
@@ -0,0 +1,13 @@
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs build-essential g++ libx11-dev libxkbfile-dev libsecret-1-dev python-is-python3 git quilt
+sudo npm install yarn
+git clone https://github.com/coder/code-server.git
+cd code-server
+git submodule update --init
+quilt push -a
+yarn
+mkdir ~/.config/code-server
+echo "bind-addr: 127.0.0.1:8080
+auth: password
+password: codeserver
+cert: false" > ~/.config/code-server/config.yaml
