76f47434766b86e91d5ddbd75892f045b7de6762
George Tian <github@georgetian.com>
2022-08-20 14:02:35 +0800

    Update install-requirements.sh

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt install -y nodejs build-essential g++ libx11-dev libxkbfile-dev libsecret-1-dev python-is-python3 git quilt
+sudo apt install -y jq nodejs build-essential g++ libx11-dev libxkbfile-dev libsecret-1-dev python-is-python3 git quilt
 sudo npm install yarn --global
 git clone https://github.com/coder/code-server.git
 cd code-server
