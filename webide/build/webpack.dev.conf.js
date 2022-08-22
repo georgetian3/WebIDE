0fe7849804b41e79a2be357c9827c653895c2f53
KennyL0423 <kennyliang2002@163.com>
2022-08-22 16:18:04 +0800

    allowedhost

--- webide/build/webpack.dev.conf.js
+++ webide/build/webpack.dev.conf.js
@@ -22,6 +22,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    allowedHosts: "all" ,
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
